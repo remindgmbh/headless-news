4f83d47c30e45f42995835a0c39e9303b3027ea5
David Mellen <d.mellen@remind.de>
2024-02-14 14:35:42 +0100

    feat: add configuration for solr

---
+++ ext_tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE tx_news_domain_model_news (
+    tx_headless_news_search_image int(11) unsigned DEFAULT '0' NOT NULL,
+    tx_headless_news_no_search tinyint(3) unsigned DEFAULT '0' NOT NULL,
+);
